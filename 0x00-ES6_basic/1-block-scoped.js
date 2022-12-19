bee6a1833484becf98c7e72cd5f084a9d8887cd1
Taura254 <benjaminfondo01@gmail.com>
2022-12-19 14:05:29 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
+
   if (trueOrFalse) {
-    // eslint-disable-next-line
-    const task = true;
-    // eslint-disable-next-line
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
